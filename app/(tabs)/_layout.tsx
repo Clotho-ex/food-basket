df9bad301ad4ba1245c238c981080f566fe499e7
Yusufcan Var <yusufcanvar90@gmail.com>
2025-10-22 01:43:55 +0300

    feat: update dependencies and enhance tab navigation layout
    - Updated various dependencies including expo, react-native, and @sentry/react-native for improved performance and stability.
    - Refactored TabsLayout component to include a custom tab bar with icons and labels for better user navigation experience.
    - Integrated MaterialCommunityIcons for tab icons and improved styling for the tab bar.
    - Enhanced authentication flow by ensuring redirection to sign-in for unauthenticated users.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,110 @@
 import useAuthStore from "@/store/auth.store";
-import { Redirect, Slot } from "expo-router";
+import MaterialCommunityIcons from "@expo/vector-icons/build/MaterialCommunityIcons";
+import cn from "clsx";
+import { Redirect, Tabs } from "expo-router";
 import React from "react";
+import { Text, View } from "react-native";
+
+interface TabBarIconProps {
+  name: keyof typeof MaterialCommunityIcons.glyphMap;
+  focused: boolean;
+  title: string;
+}
+
+function TabBarIcon({ name, focused, title }: TabBarIconProps) {
+  return (
+    <View className="tab-icon">
+      <MaterialCommunityIcons
+        name={name}
+        size={24}
+        resizeMode="contain"
+        tintColor={focused ? "#FE8C00" : "#878787"}
+        color={focused ? "#FE8C00" : "#878787"}
+      />
+      <Text
+        className={cn(
+          "font-quicksand-bold text-sm",
+          focused ? "text-primary" : "text-gray-200",
+        )}
+      >
+        {title}
+      </Text>
+    </View>
+  );
+}
 
 export default function TabsLayout() {
   const { isAuthenticated } = useAuthStore();
 
   if (!isAuthenticated) return <Redirect href="/sign-in" />;
 
-  return <Slot />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          borderTopLeftRadius: 50,
+          borderTopRightRadius: 50,
+          borderBottomLeftRadius: 50,
+          borderBottomRightRadius: 50,
+          marginHorizontal: 20,
+          height: 80,
+          position: "absolute",
+          bottom: 40,
+          backgroundColor: "white",
+          shadowColor: "#1a1a1a",
+          shadowOffset: { width: 0, height: 1 },
+          shadowOpacity: 0.25,
+          shadowRadius: 1,
+          elevation: 5,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon name="home-outline" focused={focused} title="Home" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon name="magnify" focused={focused} title="Search" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: "Cart",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon name="cart-outline" focused={focused} title="Cart" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              name="account-outline"
+              focused={focused}
+              title="Profile"
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
